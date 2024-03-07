cfb71443b2ea352586d15c99729735ec0cc873d9
lgp0509 <lgp0509@qq.com>
2024-03-07 16:56:26 +0800

    Update export.groovy

--- qupath_groovy/export.groovy
+++ qupath_groovy/export.groovy
@@ -1,1 +1,27 @@
-trans_to_detect_glo.py
+// --- remove measurements, not needed but makes file smaller
+Set annotationMeasurements = []
+
+getDetectionObjects().each{it.getMeasurementList().getMeasurementNames().each{annotationMeasurements << it}}
+//println(annotationMeasurements)
+
+annotationMeasurements.each{ removeMeasurements(qupath.lib.objects.PathCellObject, it);}
+
+
+// write to file
+boolean prettyPrint = true // false results in smaller file sizes and thus faster loading times, at the cost of nice formating
+def gson = GsonTools.getInstance(prettyPrint)
+def annotations = getAnnotationObjects()
+println gson.toJson(annotations) // you can check here but this will be HUGE and take a long time to parse
+
+
+// automatic output filename, otherwise set explicitly
+String imageLocation = getCurrentImageData().getServer().getPath()
+outfname = imageLocation.split("file:/")[1]+".json"
+outfname = outfname.replace("%20", " ")
+
+
+
+File file = new File(outfname)
+file.withWriter('UTF-8') {
+    gson.toJson(annotations,it)
+}
